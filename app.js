20948f71c956ea48b26358a04a428621848da9d4
Tahsin Ahmed <tahsinahmed21@gmail.com>
2017-05-20 20:12:56 -0500

    made some more changes

--- app.js
+++ app.js
@@ -1,12 +1,17 @@
-var userName = 'Tahsin',
-    greeting = document.getElementById('greeting'),
+var greeting = document.getElementById('greeting'),
     navDiv = document.getElementById('nav-div'),
     colorSwitcher = document.getElementById('color-switcher'),
-    footer = document.getElementsByTagName('footer')[0];
+    footer = document.getElementsByTagName('footer')[0],
+    navToggle = document.getElementById('nav-toggle');
+
+navToggle.style.display = 'none';
+navDiv.style.display = 'none';
+colorSwitcher.style.display = 'none';
+footer.style.display = 'none';
+
+var userName = prompt("What is your name?");
 
 greeting.innerHTML = 'Hello, ' + userName + '!';
-
-var navToggle = document.getElementById('nav-toggle');
 
 navToggle.addEventListener('click', toggleNavMenu);
 
@@ -29,6 +34,7 @@ function generateNav() {
     this.parentNode.removeChild(this);
     
     navDiv.style.display = 'inherit';
+    navToggle.style.display = 'inherit';
 }
 
 genLinks[1].addEventListener('click', generateColorSwitcher);
@@ -37,4 +43,12 @@ function generateColorSwitcher() {
     this.parentNode.removeChild(this);
     
     colorSwitcher.style.display = 'inherit';
+}
+
+genLinks[2].addEventListener('click', generateFooter);
+
+function generateFooter() {
+    this.parentNode.removeChild(this);
+    
+    footer.style.display = 'inherit';
 }
